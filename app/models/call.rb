dc4b1a62514876a782b8878f5f35b9067145f3fa
Matthieu Allegre <matthieu.allegre@gmail.com>
2016-10-07 13:53:04 +0200

    Remove some validations in Call

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,8 +1,6 @@
 class Call < ActiveRecord::Base
   belongs_to :user
 
-  validates :caller, presence: true # TODO: validate format
-  # validates :voicemail # TODO: validate format
-  validates :user, presence: true, allow_nil: true
-  validates :call_uuid, presence: true # TODO: validate format
+  validates :caller, presence: true
+  validates :call_uuid, presence: true
 end
